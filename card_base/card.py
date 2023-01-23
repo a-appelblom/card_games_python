ae6516e13383f6e5aeaa0929eb44d68ebdd2b832
Anton Appelblom <a.appelblom@gmail.com>
2023-01-23 21:09:35 +0100

    High low solution

--- card_base/card.py
+++ card_base/card.py
@@ -5,7 +5,7 @@ from enum import Enum
 
 
 class CardValue(Enum):
-    ACE = "ACE"
+    ACE = "Ace"
     TWO = "2"
     THREE = "3"
     FOUR = "4"
@@ -15,9 +15,25 @@ class CardValue(Enum):
     EIGHT = "8"
     NINE = "9"
     TEN = "10"
-    KNIGHT = "Knight"
+    JACK = "Jack"
     QUEEN = "Queen"
     KING = "King"
+
+
+class CardValueInt(Enum):
+    ACE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    TEN = 10
+    JACK = 11
+    QUEEN = 12
+    KING = 13
 
 
 class CardSuit(Enum):
@@ -27,14 +43,20 @@ class CardSuit(Enum):
     CLUBS = "♣"
 
 
-@dataclass
 class Card:
     suit: CardSuit
     value: CardValue
+    int_value: CardValueInt
 
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
+        self.int_value = CardValueInt[self.value.name]
 
     def __str__(self):
         return f"{self.suit.value} {self.value.value}"
+
+
+if __name__ == "__main__":
+    card = Card(CardValue.KING, CardSuit.HEARTS)
+    print(card.__dict__)
--- higher_lower/game.py
+++ higher_lower/game.py
@@ -1,5 +1,5 @@
 from card_base.deck import Deck
-from card_base.card import Card
+from card_base.card import Card, CardValue
 
 
 def higher_lower():
@@ -9,5 +9,59 @@ def higher_lower():
 
 
 class HighLowGame:
+    game_over = False
+    deck: Deck = Deck()
+    active_card: Card
+    new_card: Card
+    score = 0
+
+    def __init__(self):
+        self.deck.reset_deck()
+        self.deck.shuffle()
+        self.active_card = self.deck.pull_card()
+
     def start_game(self):
+        self.__init__()
         print("Welcome to higher lower, lets play")
+        while not self.game_over:
+            self.run_round()
+
+    def run_round(self):
+        print(f"Current card: {self.active_card}")
+        self.new_card = self.deck.pull_card()
+        guess = self.make_guess()
+        win = self.compare_card(guess)
+        if win:
+            self.win_round()
+        else:
+            self.lose_round()
+
+    def compare_card(self, guess: str) -> bool:
+        if self.new_card.value == CardValue.ACE or self.new_card.value == CardValue.ACE:
+            return True
+        elif self.new_card.int_value.value >= self.active_card.int_value.value and guess == "h":
+            return True
+        elif self.new_card.int_value.value <= self.active_card.int_value.value and guess == "l":
+            return True
+        else:
+            return False
+
+    def make_guess(self) -> str:
+        while True:
+            guess = input("Will it be higher or lower (h/l)?: ")
+            guess = guess.strip().lower()
+            if guess == "h" or guess == "l":
+                return guess
+            else:
+                print("Enter either 'h' or 'l' ")
+                continue
+
+    def win_round(self):
+        self.score += 1
+        self.active_card = self.new_card
+        print(f"Correct! Your current score is: {self.score}")
+
+    def lose_round(self):
+        self.game_over = True
+        print(
+            f"Sorry the card was {self.new_card}, you lost the game at a score of {self.score}")
